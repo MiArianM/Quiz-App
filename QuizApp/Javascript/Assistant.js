0be5215da388fafa916b94cd20091d3b6f77a879
Arian <cod16duty@gmail.com>
2024-03-13 14:46:38 +0330

    Add files via upload

---
+++ QuizApp/Javascript/Assistant.js
@@ -0,0 +1,19 @@
+let randomPushing;
+const FormattedData = (Data) => {
+  console.log(Data);
+  try {
+    const formatted = Data.map((aData) => {
+      randomPushing = Math.floor(Math.random() * 4);
+      let answers = [...aData.incorrect_answers];
+      answers.splice(randomPushing, 0, aData.correct_answer);
+      const formattedlist = {
+        Question: aData.question,
+        answers: answers,
+        CorrectAnswer: aData.correct_answer,
+      };
+      return formattedlist;
+    });
+    return formatted;
+  } catch (error) {}
+};
+export default FormattedData;
