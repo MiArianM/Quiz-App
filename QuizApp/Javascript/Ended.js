7f5f4701a1ac7b6e9fafd17079e0b77518eda1a0
Arian <cod16duty@gmail.com>
2024-03-15 04:01:14 +0330

    Add files via upload

---
+++ QuizApp/Javascript/Ended.js
@@ -0,0 +1,33 @@
+const usscored = document.getElementById("userscore");
+const username = document.getElementById("UsernamePlace");
+const saveuser = document.getElementById("saving");
+const Playo = document.getElementById("Playing");
+const Firsto = document.getElementById("FirstPage");
+const Userscore = JSON.parse(localStorage.getItem("UserScore"));
+let Scores = JSON.parse(localStorage.getItem("AllScores")) || [];
+usscored.innerText = Userscore;
+const saveTheUser = () => {
+  let UserName = username.value;
+  if (!UserName || !usscored) {
+    alert("Please Enter a Valid Username");
+  } else {
+    const FinalsScores = { name: UserName, Userscore };
+    Scores.push(FinalsScores);
+    Scores.sort((a, b) => b.Userscore - a.Userscore);
+    Scores.slice(10);
+    localStorage.setItem("AllScores", JSON.stringify(Scores));
+    localStorage.removeItem("UserScore");
+    username.value = "";
+    window.location.assign("../htmls/index.html");
+  }
+};
+saveuser.addEventListener("click", saveTheUser);
+Playo.addEventListener("click", () => {
+  localStorage.removeItem("UserScore");
+  window.location.assign("../htmls/GameTime.html");
+});
+Firsto.addEventListener("click", () => {
+  localStorage.removeItem("UserScore");
+  window.location.assign("../htmls/index.html");
+});
+console.log(localStorage);
